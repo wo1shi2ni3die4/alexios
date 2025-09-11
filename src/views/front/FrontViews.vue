636ea8db999c59835cdf6da6fdedef78f274305d
曾强 <3337622768@qq.com>
2025-09-11 16:16:18 +0800

    第三次

--- src/views/front/FrontViews.vue
+++ src/views/front/FrontViews.vue
@@ -1,54 +1,122 @@
 <template>
-<div>
-<el-container>
-  <el-header>Header</el-header>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-container>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+  <div>
+    <el-container >
+      <el-header height="300px">
+        <el-row>
+          <el-col :span="24"
+            ><div class="grid-content bg-purple-dark"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="12"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="8"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+      </el-header>
+      <el-container >
+        <el-aside >Aside</el-aside>
+        <el-container height="auto">
+          <el-main>Main</el-main>
+          <el-footer >Footer</el-footer>
+        </el-container>
+      </el-container>
     </el-container>
-  </el-container>
-</el-container>
-</div>
+  </div>
 </template>
 <script>
-export default {
-    
-  };
+export default {};
 </script>
 <style>
-.el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+.el-header,
+.el-footer {
+  background-color: #f0180dff;
+  color: #f4eaea;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #e6dad3ff;
+  color: #130a0aff;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3ff;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
