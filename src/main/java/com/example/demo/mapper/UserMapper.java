9f8d3439206622164f4d338e84c41304c4f3668b
曾强 <3337622768@qq.com>
2025-09-23 18:27:44 +0800

    第二次

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,20 +1,16 @@
 package com.example.demo.mapper;
 
 import com.example.demo.pojo.User;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    List<User> list(String name, Short email, String address);
+    List<User> list(@Param("name") String name, @Param("email") String email, @Param("address") String address);
 
     void delete(List<Integer> ids);
-    @Insert("insert into user(name, username,username,password,nickname,image,email,phone,adress,create_time,update_time) values" +
+    @Insert("insert into user(name, username,password,nickname,image,email,phone,address,create_time,update_time) values" +
             "(#{name},#{username},#{password},#{nickname},#{image},#{email},#{phone},#{address},#{createTime},#{updateTime}) ")
     void insert(User user);
     @Select("select * from user where id=#{id}")
@@ -23,6 +19,4 @@ public interface UserMapper {
     void put(User user);
     @Select("select * from user where username=#{username} and password=#{password}")
     User getByUsernameAndPassword(User user);
-
-    List<User> list(String username, String email, String address);
 }
