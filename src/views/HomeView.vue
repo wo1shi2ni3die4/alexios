02adf42e1af6ac4d1f78cfb19614f7299e730548
曾强 <3337622768@qq.com>
2025-09-24 19:38:52 +0800

    第五次

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -134,10 +134,8 @@
             <el-button class="ml-5" type="primary">搜索</el-button>
           </div>
           <div style="margin: 10px 0; font-size: 14px">
-            <el-button type="primary">新增<i class="el-icon-circle-plus-outline"></i></el-button>
+            <el-button type="primary" @click="handleAdd">新增<i class="el-icon-circle-plus-outline"></i></el-button>
             <el-button type="danger">批量删除<i class="el-icon-delete"></i></el-button>
-            <el-button type="primary">导出<i class="el-icon-download"></i></el-button>
-            <el-button type="primary">导入<i class="el-icon-upload"></i></el-button>
           </div>
           <el-table :data="tableData" border stripe :header-cell-class-name="headerclass">
             <el-table-column prop="name" label="姓名" width="140" align="center">
@@ -175,6 +173,67 @@
         </el-main>
       </el-container>
     </el-container>
+    <!-- 新增用户对话框 -->
+    <el-dialog
+        title="新增用户"
+        :visible.sync="dialogFormVisible"
+        width="600px"
+        @close="resetForm"
+    >
+      <el-form :model="form" :rules="rules" ref="form" label-width="80px">
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="姓名" prop="name">
+              <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="昵称" prop="nickname">
+              <el-input v-model="form.nickname" placeholder="请输入昵称"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="电话" prop="phone">
+              <el-input v-model="form.phone" placeholder="请输入电话"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+        </el-form-item>
+
+        <el-form-item label="地址" prop="address">
+          <el-input v-model="form.address" placeholder="请输入地址"></el-input>
+        </el-form-item>
+
+        <el-form-item label="头像">
+          <el-upload
+              class="avatar-uploader"
+              action="/upload"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="form.image" :src="form.image" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"  style="display: flex; justify-content: center; align-items: center;"></i>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitForm">确定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <style>
@@ -202,7 +261,33 @@ export default {
       sideWidth: 200,
       logoTextshow: true,
       headerclass: "headerclass",
-      
+      // 对话框显示控制
+      dialogFormVisible: false,
+
+      // 表单数据
+      form: {
+        name: '',
+        username: '',
+        nickname: '',
+        image: '',
+        email: '',
+        phone: '',
+        address: ''
+      },
+
+      // 表单验证规则
+      rules: {
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
+        ],
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+        ]
+      },
       // 分页参数
       pagination: {
         currentPage: 1,
@@ -225,7 +310,7 @@ export default {
   created() {
     this.loadData()
   },
-  
+
   methods: {
     collapse() {
       this.isCollapse = !this.isCollapse;
@@ -288,20 +373,14 @@ export default {
     handleSelectionChange(selection) {
       this.selectedRows = selection
     },
-    
-    // 新增用户
-    handleAdd() {
-      this.$message.info('新增功能待实现')
-      // 这里可以打开新增对话框
-    },
-    
+
     // 编辑用户
     handleEdit(row) {
       this.$message.info(`编辑用户：${row.name}`)
       // 这里可以打开编辑对话框，并加载用户详情
       // this.getUserDetail(row.id)
     },
-    
+
     // 删除用户
     async handleDelete(id) {
       try {
@@ -352,7 +431,97 @@ export default {
       } catch (error) {
         this.$message.error('获取用户详情失败：' + error.message)
       }
+    },
+    // 打开新增对话框
+    handleAdd() {
+      this.dialogFormVisible = true;
+      this.resetForm();
+    },
+
+    // 重置表单
+    resetForm() {
+      this.form = {
+        name: '',
+        username: '',
+        nickname: '',
+        image: '',
+        email: '',
+        phone: '',
+        address: ''
+      };
+      if (this.$refs.form) {
+        this.$refs.form.clearValidate();
+      }
+    },
+
+    // 提交表单
+    async submitForm() {
+      try {
+        // 表单验证
+        await this.$refs.form.validate();
+
+        // 调用新增接口
+        await request.post('/user', this.form);
+
+        this.$message.success('新增用户成功');
+        this.dialogFormVisible = false;
+        this.loadData(); // 刷新表格数据
+      } catch (error) {
+        if (error !== 'cancel') {
+          this.$message.error('新增失败：' + error.message);
+        }
+      }
+    },
+
+    // 头像上传成功
+    handleAvatarSuccess(response) {
+      if (response && response.code === 1) {
+        this.form.image = response.data;
+        this.$message.success('头像上传成功');
+      } else {
+        this.$message.error('头像上传失败');
+      }
+    },
+
+    // 头像上传前校验
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg' || file.type === 'image/png';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('头像只能是 JPG 或 PNG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('头像大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
     }
   }
 };
 </script>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+
+</style>
